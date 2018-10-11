8147da02be8d830d934c044812744f39e136531c
Uri <oriol.castroarnau@gmail.com>
2018-10-11 12:48:34 +0200

    Initial implementation

--- src/components/ideasform.js
+++ src/components/ideasform.js
@@ -1,50 +1,103 @@
-import React from 'react'
-import { Container, Form, Checkbox, Message, Header } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Container, Form, Message, Header } from 'semantic-ui-react'
+import axios from 'axios'
+import styles from './ideasform.module.scss'
 
-const IdeasForm = () => (
-  <div id="formulari" style={{ marginBottom: '40px' }}>
-    <Container text>
-      <Header as="h2">Digues la teva</Header>
-      <Form success action="la url de la function" method="POST">
-        <Message
-          success
-          header="Proposta enviada"
-          content="Gracies per la teva participació. Rebràs un correu electronic confirmant que hem rebut la proposta"
-        />
-        <Form.Group widths="equal">
-          <Form.Input
-            label="Nom"
-            name="name"
-            placeholder="Carpa Juanita"
-            type="text"
-            required
-            fluid
-          />
-          <Form.Input
-            label="Correu electrònic"
-            name="email"
-            placeholder="juani@curiositats.mar"
-            type="email"
-            required
-            fluid
-          />
-        </Form.Group>
-        <Form.TextArea
-          label="Fes la teva proposta"
-          name="message"
-          placeholder="Jo crec que Vilanova necessita..."
-          required
-          autoHeight
-        />
-        <Form.Field>
-          <Checkbox label="Estic d'acord amb les condicions de privacitat" />
-        </Form.Field>
-        <Form.Button type="submit" color="black">
-          Envia
-        </Form.Button>
-      </Form>
-    </Container>
-  </div>
-)
+class IdeasForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      hasConfirmedPrivacy: false,
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit(event) {
+    const data = this.state
+    console.log(data)
+    axios
+      .post('/.netlify/functions/send_idea', data)
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+    //Al final fer setState a blanc
+  }
+
+  handleChange(event) {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return (
+      <div id="formulari" style={{ marginBottom: '40px' }}>
+        <Container text>
+          <Header as="h2">Digues la teva</Header>
+          <Form success onSubmit={this.handleSubmit}>
+            <Message
+              success
+              header="Proposta enviada"
+              content="Gracies per la teva participació. Rebràs un correu electronic confirmant que hem rebut la proposta"
+            />
+            <Form.Group widths="equal">
+              <Form.Input
+                label="Nom"
+                name="name"
+                placeholder="Carpa Juanita"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChange}
+                required
+                fluid
+              />
+              <Form.Input
+                label="Correu electrònic"
+                name="email"
+                placeholder="juani@curiositats.mar"
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+                fluid
+              />
+            </Form.Group>
+            <Form.TextArea
+              label="Fes la teva proposta"
+              name="message"
+              placeholder="Jo crec que Vilanova necessita..."
+              value={this.state.message}
+              onChange={this.handleChange}
+              required
+              autoHeight
+            />
+            <Form.Field
+              label="bla bla bla"
+              type="checkbox"
+              control="input"
+              name="hasConfirmedPrivacy"
+              value={this.state.hasConfirmedPrivacy}
+              onChange={this.handleChange}
+              className={styles.myCheckbox}
+            />
+
+            <Form.Button type="submit" color="black">
+              Envia
+            </Form.Button>
+          </Form>
+        </Container>
+      </div>
+    )
+  }
+}
 
 export default IdeasForm
--- src/functions/send_idea.js
+++ src/functions/send_idea.js
@@ -1,14 +1,59 @@
-exports.handler = async (event, context) => {
+import nodemailer from 'nodemailer'
+import mg from 'nodemailer-mailgun-transport'
+import querystring from 'querystring'
+require('dotenv').config()
+
+var auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+}
+
+var nodemailerMailgun = nodemailer.createTransport(mg(auth))
+
+exports.handler = async (event, context, callback) => {
   // Only allow POST
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
+  const params = querystring.parse(event.body)
+  const email = params.email
+  const name = params.name
+  const message = params.message
 
-  return {
-    statusCode: 200,
-    body: 'Hello, World',
-  }
+  return nodemailerMailgun.sendMail(
+    {
+      from: 'myemail@example.com',
+      to: { email },
+      subject: 'Hey you, awesome!',
+      'h:Reply-To': 'reply2this@company.com',
+      //You can use "html:" to send HTML email content. It's magic!
+      html: `<div><h3>Message from ${name}</h3><b>${message}</b></div>`,
+      //You can use "text:" to send plain-text content. It's oldschool!
+      text: 'HEY!',
+    },
+    (error, info) => {
+      if (error) {
+        const response = {
+          statusCode: 500,
+          body: JSON.stringify({
+            error: error.message,
+          }),
+        }
+        callback(null, response)
+      }
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `email_delivered`,
+        }),
+      }
+      callback(null, response)
+    }
+  )
 }
+
 //Afegir gestió de dotenv per credencials mailgun
-//Afegir package mailer per enviar els correus
+//Afegir package nodemailer per enviar els correus
 //Afegir status return per mostrar succes message al client
