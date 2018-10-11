9d887ee9ec79fa5074309008756c22c2e961e5b2
Uri <oriol.castroarnau@gmail.com>
2018-10-11 16:33:25 +0200

    More testing

--- src/functions/send-mail.js
+++ src/functions/send-mail.js
@@ -1,15 +1,51 @@
 import querystring from 'querystring'
+import nodemailer from 'nodemailer'
+import mg from 'nodemailer-mailgun-transport'
+require('dotenv').config()
 
 exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
 
-  const params = querystring.parse(event.body)
-  const email = params.email || 'test@test.com'
-
-  return {
-    statusCode: 200,
-    body: `Email sended to ${email}`,
+  var auth = {
+    auth: {
+      api_key: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
   }
+
+  var nodemailerMailgun = nodemailer.createTransport(mg(auth))
+
+  const params = querystring.parse(event.body)
+  const name = params.name || 'Test'
+  const email = params.email || 'test@test.com'
+  const message = params.message || 'This is a test'
+
+  console.log(name, email, message)
+
+  nodemailerMailgun.sendMail(
+    {
+      from: 'myemail@example.com',
+      to: { email },
+      subject: 'Hey you, awesome!',
+      'h:Reply-To': 'reply2this@company.com',
+      //       //You can use "html:" to send HTML email content. It's magic!
+      html: `<div><h3>Message from ${name}</h3><b>${message}</b></div>`,
+      //       //You can use "text:" to send plain-text content. It's oldschool!
+      text: 'Hola!',
+    },
+    error => {
+      if (error) {
+        return {
+          statusCode: 500,
+          body: SON.stringify({ error: error.message }),
+        }
+      }
+      return {
+        statusCode: 200,
+        body: 'email_delivered',
+      }
+    }
+  )
 }
