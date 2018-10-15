68fc745445f6a7eedc619a37cd449446bd3f92f8
Uri <oriol.castroarnau@gmail.com>
2018-10-15 14:34:30 +0200

    Solve mail delivery error

--- src/functions/send_mail.js
+++ src/functions/send_mail.js
@@ -27,7 +27,7 @@ exports.handler = function(event, context, callback) {
     : ''
 
   var mailOptions = {
-    from: '"CUP Vilanova i la Geltrú" <vilanova@cup.cat>',
+    from: '"CUP Vilanova i la Geltrú" <contacte@estemapuntvng.cat>',
     subject: `Moltes gràcies ${name} per la proposta`,
     html: `<div><p>Moltes gràcies ${name} per la teva participació!</p><p>Hem rebut la teva proposta a través del web www.estemapuntvng.cat. La tindrem en compte a l'hora d'elaborar el programa.</p><p style="font-weight:bold">Proposta:</p><p style="font-style:italic">${message}</p><p>Informació de privacitat:<p><ul>${subscribeCampaignInfo}${subscribeGeneralInfo}</ul></div>`,
     to: `"${name}" ${email}`,
