df2f03fb01e198276e8b3670b62df556deb77012
Uri <oriol.castroarnau@gmail.com>
2018-10-15 15:14:55 +0200

    Solve mailgun error

--- src/functions/send_mail.js
+++ src/functions/send_mail.js
@@ -32,7 +32,7 @@ exports.handler = function(event, context, callback) {
     subject: `Moltes gràcies ${name} per la proposta`,
     html: `<div><p>Moltes gràcies ${name} per la teva participació!</p><p>Hem rebut la teva proposta a través del web www.estemapuntvng.cat. La tindrem en compte a l'hora d'elaborar el programa.</p><p style="font-weight:bold">Proposta:</p><p style="font-style:italic">${message}</p><p>Informació de privacitat:<p><ul>${subscribeCampaignInfo}${subscribeGeneralInfo}</ul></div>`,
     to: `"${name}" ${email}`,
-    bbc: 'oriol.castroarnau@gmail.com',
+    bcc: 'cupdevilanova@gmail.com',
   }
 
   var transporter = nodemailer.createTransport(mg(auth))
