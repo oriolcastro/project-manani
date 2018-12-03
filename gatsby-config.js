80e0a5f304bab0a7c9da7375aea746527ea8ed3f
Uri <oriol.castroarnau@gmail.com>
2018-12-03 14:35:05 +0100

    Update dependencies and add new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,16 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     'gatsby-plugin-netlify',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      },
+    },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
